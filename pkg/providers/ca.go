3b10b115ddf1f50c3245c833b8a1c81bd5720ce8
Tamal Saha <tamal@appscode.com>
2024-10-24 07:57:27 -0800

    Load clusterissuer secret from cert-manager namespace (#35)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/providers/ca.go
+++ pkg/providers/ca.go
@@ -27,6 +27,8 @@ import (
 	cmapi "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
 	"gomodules.xyz/cert"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"kmodules.xyz/client-go/meta"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -37,20 +39,35 @@ type IssuerProvider struct {
 var _ lib.CAProvider = &IssuerProvider{}
 
 func (c *IssuerProvider) GetCAs(obj client.Object, _ string) ([]*x509.Certificate, error) {
+	kind := obj.GetObjectKind().GroupVersionKind().Kind
+	issuerKey, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		return nil, err
+	}
+
 	issuer, ok := obj.(cmapi.GenericIssuer)
 	if !ok {
-		return nil, fmt.Errorf("%v %s/%s is not a GenericIssuer", obj.GetObjectKind().GroupVersionKind(), obj.GetNamespace(), obj.GetName())
+		return nil, fmt.Errorf("%s %s is not a GenericIssuer", kind, issuerKey)
 	}
+
 	if issuer.GetSpec().CA == nil {
-		return nil, fmt.Errorf("%v %s/%s does not have a CA", issuer.GetObjectKind().GroupVersionKind(), issuer.GetNamespace(), issuer.GetName())
+		return nil, fmt.Errorf("%s %s does not have a CA", kind, issuerKey)
 	}
 
 	var secret corev1.Secret
 	secretRef := client.ObjectKey{
-		Namespace: issuer.GetNamespace(),
-		Name:      issuer.GetSpec().CA.SecretName,
+		Namespace: func() string {
+			if kind == "ClusterIssuer" {
+				// cert-manager requires the ClusterIssuer ca secret to be in the same namespace where it is deployed.
+				// So, csi-driver must be in the same namespace where cert-manager is installed.
+				// ns will be defaulted to cert-manager namespace in standard deployments.
+				return meta.PodNamespace()
+			}
+			return issuer.GetNamespace()
+		}(),
+		Name: issuer.GetSpec().CA.SecretName,
 	}
-	err := c.Reader.Get(context.TODO(), secretRef, &secret)
+	err = c.Reader.Get(context.TODO(), secretRef, &secret)
 	if err != nil {
 		return nil, err
 	}
@@ -64,16 +81,16 @@ func (c *IssuerProvider) GetCAs(obj client.Object, _ string) ([]*x509.Certificat
 		return nil, err
 	}
 	if len(caCerts) == 0 {
-		return nil, fmt.Errorf("%v %s/%s signing certificate is not a CA", issuer.GetObjectKind().GroupVersionKind(), issuer.GetNamespace(), issuer.GetName())
+		return nil, fmt.Errorf("%s %s signing certificate is not a CA", kind, issuerKey)
 	}
 
 	now := time.Now()
 	for _, caCert := range caCerts {
 		if now.Before(caCert.NotBefore) {
-			return nil, fmt.Errorf("%v %s/%s points a CA cert not valid before %v, now: %s", issuer.GetObjectKind().GroupVersionKind(), issuer.GetNamespace(), issuer.GetName(), caCert.NotBefore, now)
+			return nil, fmt.Errorf("%s %s points a CA cert not valid before %v, now: %s", kind, issuerKey, caCert.NotBefore, now)
 		}
 		if now.After(caCert.NotAfter) {
-			return nil, fmt.Errorf("%v %s/%s points a CA cert expired at %v, now: %s", issuer.GetObjectKind().GroupVersionKind(), issuer.GetNamespace(), issuer.GetName(), caCert.NotAfter, now)
+			return nil, fmt.Errorf("%s %s points a CA cert expired at %v, now: %s", kind, issuerKey, caCert.NotAfter, now)
 		}
 	}
 
